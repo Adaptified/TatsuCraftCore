bebd9d954fa0d82b0f95682d32af18f545811e09
theLAZYway01 <hentscheljr1@aol.com>
2015-03-05 12:46:29 -0500

    Added PROMOTE
    To enable /promote, added message PROMOTE.
    
    Not to be removed without asking:
    
    @GotNotFriends
    @VjGamingUK2000
    @theLAZYway01

--- src/me/BrandonSteed/ShadowEra/SE_Messages.java
+++ src/me/BrandonSteed/ShadowEra/SE_Messages.java
@@ -6,6 +6,7 @@ public class SE_Messages {
    public static final String NO_PLAYER = ChatColor.RED + "That username cannot be found. Ensure your spelling is correct.";
    public static final String NO_PREMS = ChatColor.DARK_RED + "You don't have the required permissions to execute this command.";
    public static final String USAGE = ChatColor.GRAY + "The proper usage for this command is: ";
+   public static final String PROMOTE = ChatColor.GREEN + " Has been promoted to the rank";
    public static final String SWEAR = ChatColor.GRAY + "Swearing is forbidden!";
    public static final String CHILDREN_FRIENDLY_PLEASE = ChatColor.DARK_RED + "Hi there, we have seven year olds playing here- I'd love if you didn't say 'those type' of words.  Children friendly, please!";
    public static final String FORUMS_URL = "http://shadowera.boards.net";
